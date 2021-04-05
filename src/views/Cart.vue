5f435bf8671a01f41e52f0895e42456689e691b6
Aaron Woodbury <aarondswoodbury@gmail.com>
2021-04-05 17:59:32 -0600

    init commit

---
+++ src/views/Cart.vue
@@ -0,0 +1,58 @@
+<template>
+<div>
+  <div class="wrapper">
+  </div>
+  <h1 style="text-align:center">Cart</h1>
+  <hr>
+  <CartList :products="products" />
+</div>
+</template>
+
+<script>
+import CartList from "../components/CartList.vue"
+export default {
+  name: 'Cart',
+  components: {
+    CartList
+  },
+  computed: {
+    products() {
+      return this.$root.$data.cart;
+    }
+  },
+}
+</script>
+
+<style scoped>
+.wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.search {
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  width: 50%;
+}
+
+form {
+  display: table;
+  width: 100%;
+}
+
+i {
+  display: table-cell;
+  padding-left: 10px;
+  width: 1px;
+}
+
+input {
+  display: table-cell;
+  font-size: 20px;
+  border: none !important;
+  box-shadow: none !important;
+  width: 100%;
+  height: 40px;
+}
+</style>
